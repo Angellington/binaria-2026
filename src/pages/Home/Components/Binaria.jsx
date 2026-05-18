9ab2cf47832da38ae32b83897f01df13f77cae4a
Angellington <superetro85@gmail.com>
2026-05-18 05:38:37 -0300

    chart style

--- src/pages/Home/Components/Binaria.jsx
+++ src/pages/Home/Components/Binaria.jsx
@@ -12,6 +12,10 @@ const Binaria = () => {
         <Box className={style.ordinates} />
         <Box className={style.ordinatesH} />
 
+        <Box className={style.absiscissasA1} />
+        <Box className={style.absiscissasA2} />
+        <Box className={style.absiscissasBottom} />
+
         {/* DOTLINES  */}
         <Box className={style.dashed45} />
 
@@ -26,10 +30,16 @@ const Binaria = () => {
 
         {/* Texts  */}
         <Box className={style.heightGraphicLetter}>h</Box>
+        <Box className={style.A1GraphicLetter}>a1</Box>
+        <Box className={style.A2GraphicLetter}>a2</Box>
         <Box className={style.inclinationAngleLetter}>inclination angle</Box>
         <Box className={style.inclinationAngleJapanese}>軌道傾斜角 81.4°</Box>
-
-
+        <Box className={style.binarySeparation}>
+          binary separation 連星間距離 α
+        </Box>
+        <Box className={style.Hipparcos}>α=38.0 AS pc (Hipparcos)</Box>
+        <Box className={style.orbitalPlane}>orbital plane</Box>
+         <Box className={style.orbitalPlaneJapanese}>軌道面</Box>
 
         {/* <img src={Chart} className={style.chart} /> */}
       </Box>
