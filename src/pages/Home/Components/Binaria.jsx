e77d342e3aa4e3b69bb837aa1867d890ba97a403
Angellington <superetro85@gmail.com>
2026-05-20 15:58:08 -0300

    INITIAL TANAGI

--- src/pages/Home/Components/Binaria.jsx
+++ src/pages/Home/Components/Binaria.jsx
@@ -1,58 +1,56 @@
 import style from "./Binaria.module.css";
 import { Box, Tooltip } from "@mui/material";
 import Xai from "../../../assets/objects/Xai.png";
+import Yanagi from "../../../assets/objects/Yanagi.png";
 import PopoverComponent from "../../../components/Popover";
 import { useEffect, useRef, useState } from "react";
 import { startNagaoMovement } from "./animations/nagaoMovements";
 import NagaoFrame from "../../../components/Members/NagaoFrame";
 import { useGetApi } from "../../../hooks/useGetApi";
 
+
 const Binaria = () => {
-  const nagaoRef = useRef(null)
+  const nagaoRef = useRef(null);
+  const yanagiRef = useRef(null);
   const { getJsonFile } = useGetApi();
 
   useEffect(() => {
-    return startNagaoMovement(nagaoRef.current, style.nagaoOrbital)
-  }, [style.nagaoOrbital])
+    return startNagaoMovement(nagaoRef.current, style.nagaoOrbital);
+  }, [style.nagaoOrbital]);
 
-
-  const [anchorEl, setAnchorEl] = useState(null)
-  const [selectedId, setSelectedId] = useState(null)
-  const [mbdata, setMbdata] = useState(null)
-
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedId, setSelectedId] = useState(null);
+  const [mbdata, setMbdata] = useState(null);
 
   const openDetails = (e) => {
-    const id = e.currentTarget.getAttribute('id');
+    const id = e.currentTarget.getAttribute("id");
     setSelectedId(id);
-    setAnchorEl(e.currentTarget)
+    setAnchorEl(e.currentTarget);
   };
 
   const handleClosePopover = () => {
-    setAnchorEl(null)
-    setSelectedId(null)
-  }
-
+    setAnchorEl(null);
+    setSelectedId(null);
+  };
 
   useEffect(() => {
     getJsonFile("mocks/data/members.json").then((data) => {
       setMbdata(data);
     });
-  }, [])
-
+  }, []);
 
   const renderContent = (id) => {
+    console.log("mbdata", mbdata);
 
-    console.log("mbdata", mbdata)
-    
-    const member =  mbdata?.find((mb) => mb.id === id)
+    const member = mbdata?.find((mb) => mb.id === id);
 
     switch (id) {
       case "yoshisa_nagao":
-        return <NagaoFrame data={member}  />
+        return <NagaoFrame data={member} />;
       default:
         return <Box></Box>;
     }
-  }
+  };
 
   return (
     <Box className={style.binaria}>
@@ -93,22 +91,30 @@ const Binaria = () => {
         <Box className={style.orbitalPlaneJapanese}>軌道面</Box>
 
         <Tooltip title="Nagao">
-          <img src={Xai} className={style.NagaoAsteroid} id={"yoshisa_nagao"} onClick={openDetails}
-          
-          ref={nagaoRef}
+          <img
+            src={Xai}
+            className={style.NagaoAsteroid}
+            id={"yoshisa_nagao"}
+            onClick={openDetails}
+            ref={nagaoRef}
           ></img>
         </Tooltip>
 
-        {/* <img src={Chart} className={style.chart} /> */}
+        <Tooltip title="Yanagi Nagi">
+          <img
+            src={Yanagi}
+            className={style.YanagiAsteroid}
+            id={"yanaginagi"}
+            onClick={openDetails}
+            ref={yanagiRef}
+          ></img>
+        </Tooltip>
 
-      
-        <PopoverComponent
-          
-          anchorEl={anchorEl}
-          onClose={handleClosePopover}
-          >
-            {renderContent(selectedId)}
-          </PopoverComponent>
+        <img src={"assets/chart.png"} className={style.chart} />
+
+        <PopoverComponent anchorEl={anchorEl} onClose={handleClosePopover}>
+          {renderContent(selectedId)}
+        </PopoverComponent>
       </Box>
     </Box>
   );
